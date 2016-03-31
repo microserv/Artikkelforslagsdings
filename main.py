4dace3ee8d305d44a8b44f9b46be7055fdfb5ce7
WORWBAHAWS <you@example.com>
2016-03-31 16:17:53 +0200

    Sender to requests av en eller annen grunn

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from twisted.internet import reactor, defer
 from twisted.python import log
 from twisted.internet.task import deferLater
 from twisted.web.server import NOT_DONE_YET
-
-
+from twisted.internet.protocol import Factory, Protocol
+from twisted.internet.endpoints import TCP4ClientEndpoint
 import json
 
 
 import queries
+class SearchIndexClient(Protocol):
+    def connectionMade(self):
+        request = self.factory.requestquery
+        self.transport.write(request)
+        self.transport.loseConnection()
 
 #Convey searches to index and back
 class SearchServer(resource.Resource):
@@ -38,14 +43,29 @@ class SearchServer(resource.Resource):
         agent = Agent(reactor)
         print(indexquery_string)
         #{"Query": "forskrift om utdanning ", "Partial": true}
-        d = agent.request(
-            'POST',
-            'http://127.0.0.1:8001',
-            Headers({'User-Agent': ['Twisted Search'],
-                     'Accept':['application/json'], 
-                     'Content-Type':['application/json']
-                     }),
-            indexquery_string)
+        #d = agent.request(
+            #'POST',
+            #'http://127.0.0.1:8001',
+            #Headers({'User-Agent': ['Twisted Search'],
+            #         'Accept':['application/json'], 
+            #         'Content-Type':['application/json']
+            #         }),
+            #indexquery_string)
+            
+        QUERY = """
+POST / HTTP/1.1
+Host: 127.0.0.1:8001
+User-Agent: FUCKJAVA
+Content-Type: application/json
+Content-Length: %s
+
+%s}""".strip().replace('\n', '\r\n') % (len(indexquery_string), indexquery_string)
+
+        factory = Factory()
+        factory.protocol = SearchIndexClient
+        factory.requestquery = QUERY
+        point = TCP4ClientEndpoint(reactor, "127.0.0.1", 8001)
+        d = point.connect(factory)
         def asdfg(x):
             print(x)
         d.addCallback(lambda x: asdfg(x))
