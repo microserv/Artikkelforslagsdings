552e4242d6fcd986557286da571dc708b1866b51
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-04-27 19:41:31 -0400

    Make the local config use the DHT node to obtain the IP to the services, as this is what we will do when it is deployed.

--- CONFIG.py
+++ CONFIG.py
@@ -2,11 +2,11 @@ import comm
 
 comm_host = "http://127.0.0.1:9001"
 
-index_host = 'http://{}:{}/'.format(comm.get_service_ip('index', comm_host), 8001)
-spell_host = 'http://{}:{}/'.format(comm.get_service_ip('spell', comm_host), 8002)
+#index_host = 'http://{}:{}/'.format(comm.get_service_ip('index', comm_host), 8001)
+#spell_host = 'http://{}:{}/'.format(comm.get_service_ip('spell', comm_host), 8002)
 
-#index_host = "http://127.0.0.1:8001/"
-#spell_host = "http://127.0.0.1:8002/"
+index_host = "http://127.0.0.1:8001/"
+spell_host = "http://127.0.0.1:8002/"
 
 SEARCH_SERVER_PORT = 8000
 
