89617eeb0b83e5b6db45feeee532b43bc5912261
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-04-27 20:50:04 -0400

    Ensure that the service attempts to fetch the correct services.

--- CONFIG.py
+++ CONFIG.py
@@ -2,8 +2,8 @@ import comm
 
 comm_host = "http://127.0.0.1:9001"
 
-#index_host = 'http://{}:{}/'.format(comm.get_service_ip('index', comm_host), 8001)
-#spell_host = 'http://{}:{}/'.format(comm.get_service_ip('spell', comm_host), 8002)
+#index_host = 'http://{}:{}/'.format(comm.get_service_ip('indexer', comm_host), 8001)
+#spell_host = 'http://{}:{}/'.format(comm.get_service_ip('spell-check', comm_host), 8002)
 
 index_host = "http://127.0.0.1:8001/"
 spell_host = "http://127.0.0.1:8002/"
